5e94960e6d7b62cbf529c06e62a483deefbe18d5
진대윤 <zetbouaka@gmail.com>
2023-02-03 03:26:52 +0000

    해시태그 수정

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -113,17 +113,18 @@ function App() {
     
     const TwitterShare = (event) => {
         const url = encodeURI('https://maze.akaiaoon.dev/');
+        const hashtag = encodeURI('일일미궁');
         if (userState == State.힌트1봤음) {
-            const text = encodeURI('#일일미궁 오늘의 문제는 그럭저럭 쉬웠어. 너도 한번 풀어봐!');  
-            window.open(`https://twitter.com/intent/tweet?text=${text}&url=${url}&hashtags=일일미궁`);
+            const text = encodeURI('오늘의 문제는 그럭저럭 쉬웠어. 너도 한번 풀어봐!');  
+            window.open(`https://twitter.com/intent/tweet?text=${text}&url=${url}&hashtags=${hashtag}`);
         } 
         else{
             if (userState == State.힌트2봤음) {
-                const text = encodeURI('#일일미궁 오늘의 문제는 졸라 어려웠어. 너도 한번 풀어봐!');
-                window.open(`https://twitter.com/intent/tweet?text=${text}&url=${url}&hashtags=일일미궁`);
+                const text = encodeURI('오늘의 문제는 졸라 어려웠어. 너도 한번 풀어봐!');
+                window.open(`https://twitter.com/intent/tweet?text=${text}&url=${url}&hashtags=${hashtag}`);
             } else {
-                const text = encodeURI('#일일미궁 오늘의 문제는 아주 쉬웠어. 너도 한번 풀어봐!');
-                window.open(`https://twitter.com/intent/tweet?text=${text}&url=${url}&hashtags=일일미궁`);
+                const text = encodeURI('오늘의 문제는 아주 쉬웠어. 너도 한번 풀어봐!');
+                window.open(`https://twitter.com/intent/tweet?text=${text}&url=${url}&hashtags=${hashtag}`);
             }
         }        
     };
